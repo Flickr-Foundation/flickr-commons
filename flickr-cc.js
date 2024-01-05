6cbb9289d6a194139e5822268fa3d0c7aa2566c3
Jessamyn <jessamyn@gmail.com>
2024-01-05 10:08:07 -0500

    Update flickr-cc.js
    more places this shows up

--- flickr-cc.js
+++ flickr-cc.js
@@ -51,14 +51,14 @@
 		flickr.photos.search({
 			tags: query,
 			sort: 'relevance',
-			license: "7,8,9,10" // All the permissively licensed photos
+			license: "7" // All the permissively licensed photos
 		}, callback);
 	}
 
 	// You can uncomment this and check the JS console to see more details about licenses...
 	/*flickr.photos.licenses.getInfo({
 		text: query,
-		license: "7,8,9,10" // All the permissively licensed photos
+		license: "7" // All the permissively licensed photos
 	}, function(rsp) {
 		console.log(rsp);
 	});*/
